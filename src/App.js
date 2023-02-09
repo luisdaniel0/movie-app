b7edb93002c5d6c7ecf208d49ca5b486042b295f
Luis Sanchez <luisanchezdh@gmail.com>
2023-02-09 02:56:49 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import MovieCard from './components/MovieCard'
 //7fe41261
 const API_URL = 'http://www.omdbapi.com?apikey=7fe41261'
 
-const movie1 = {
-  "Title": "Superman, Spiderman or Batman",
-  "Year": "2011",
-  "imdbID": "tt2084949",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
+
 
 function App() {
 
